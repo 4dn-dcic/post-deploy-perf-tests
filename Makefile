fecb119fefa8026c7f2a2bfdd8b697c71fc66580
William Ronchetti <wrr33@cornell.edu>
2020-06-24 12:08:16 -0400

    C4-192 draft of main file

--- Makefile
+++ Makefile
@@ -11,9 +11,8 @@ test:
 	python -m unittest lib.test.test_locust_api
 
 deploy-test-ui:
-	echo "Spawnig Locust in Interactive Mode..."
-	locust -f deploy_tests/fourfront/fourfront.py
+	python main.py fourfront
 
 deploy-test-headless:
 	echo "Enable the below test when access key is configured!"
-	# locust -f deploy_tests/fourfront/fourfront.py --no-web -c 100 -r 10 --run-time 60s --print-stats
+	# python main.py fourfront --headless
---
+++ main.py
@@ -0,0 +1,48 @@
+import subprocess
+import argparse
+
+
+EPILOG = __doc__
+VALID_ENVS = {'fourfront': 'deploy_tests/fourfront/fourfront.py', 'cgap': None}
+
+
+def main():
+    parser = argparse.ArgumentParser(description='post-deploy-perf-tests entry point', epilog=EPILOG,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('app', help='which application to test - one of "fourfront" or "cgap"')
+    parser.add_argument('--headless', help='Whether or not to run in headless mode with default configuration',
+                        action='store_true', default=False)
+    args = parser.parse_args()
+    app_lower = args.app.lower()
+
+    if app_lower not in VALID_ENVS:
+        raise RuntimeError('Gave invalid env %s, expected one of %s' % (app_lower, VALID_ENVS))
+
+    locust_file_location = VALID_ENVS[app_lower]
+    if not locust_file_location:
+        raise RuntimeError('Tried to run tests on environment %s that has no configuration!' % app_lower)
+
+    if args.headless:
+        try:
+            subprocess.call([
+                'locust', '-f', locust_file_location,
+                '--no-web',
+                '-c', '100',  # number of clients
+                '-r', '10',  # hatch rate
+                '--run-time', '60s',
+                '--print-stats'
+            ])
+        except KeyboardInterrupt:
+            pass
+    else:
+        try:
+            subprocess.call([
+                'locust', '-f', locust_file_location
+            ])
+        except KeyboardInterrupt:
+            pass
+    exit(0)
+
+
+if __name__ == '__main__':
+    main()
