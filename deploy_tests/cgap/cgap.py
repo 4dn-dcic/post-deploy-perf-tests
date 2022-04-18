79d95df1167f0a0eef2947e1903932ee038e4b6f
William Ronchetti <wrr33@cornell.edu>
2022-04-18 16:07:08 -0400

    remove call

--- deploy_tests/cgap/cgap.py
+++ deploy_tests/cgap/cgap.py
@@ -20,7 +20,6 @@ class BasicUser(HttpUser):
     weight = 1
     wait_time = between(3, 5)
     _auth = HTTPBasicAuth(*locust_auth.get_username_and_password())  # get CGAP auth
-    requests.get(build_url(host, "/Case?limit=20"), auth=_auth).json()
     cases = list(c['@id'] for c in requests.get(build_url(host, "/Case?limit=20"), auth=_auth).json()['@graph'])
     vsl = list(c['@id'] for c in requests.get(build_url(host, '/VariantSampleList?limit=10'), auth=_auth).json()['@graph'])
     # These types are most data model intensive
